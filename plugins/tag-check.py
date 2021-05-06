e0ea76f114d32ade414e7ca0d389c9a07c98c93a
꧁✪≋✘_.мүsтεяισυs_.✘≋✪꧂ <73080587+XMYSTERlOUSX@users.noreply.github.com>
2021-05-06 11:21:07 +0530

    Update tag-check.py

--- plugins/tag-check.py
+++ plugins/tag-check.py
@@ -26,7 +26,7 @@ async def addorno(client, message):
     chat_id = message.chat.id
     user_id = message.from_user.id
     channel = -1001489291010
-    status = Client.get_chat_member(chat_id, message.from_user.id).status
+    user = client.get_chat_member(message.chat.id, message.from_user.id)
     REPLY_MARKUP = InlineKeyboardMarkup([
     [InlineKeyboardButton("Unmute Me ⚠️",
                           url="https://t.me/tag_check_bot?start=start")]])
